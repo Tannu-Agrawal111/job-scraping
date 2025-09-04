3a1e245b7d7a3cf2585673eed28e5107af6a1e5a
Tannu-Agrawal111 <tnnugrwl@gmail.com>
2025-09-05 00:13:56 +0530

    Initial commit - safe setup with .env

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -3,13 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBiUsjz8NVxMXF3SyD6R27jwmdUZxEt_pQ",
-  authDomain: "job-listing-web-application.firebaseapp.com",
-  projectId: "job-listing-web-application",
-  storageBucket: "job-listing-web-application.appspot.com",
-  messagingSenderId: "657881382517",
-  appId: "1:657881382517:web:a2677bca4a37b1b527423c"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
+
 
 const app = initializeApp(firebaseConfig);
 
@@ -17,5 +18,4 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { auth, db };
-
 
