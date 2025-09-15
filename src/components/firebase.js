0e2c516c8d63dc266308f20c7321d6b172519702
Tannu-Agrawal111 <tnnugrwl@gmail.com>
2025-09-15 23:07:38 +0530

    Initial commit

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,16 +1,36 @@
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID
+// };
+
+
+// const app = initializeApp(firebaseConfig);
+
+// const auth = getAuth(app);
+// const db = getFirestore(app);
+
+// export { auth, db };
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyBiUsjz8NVxMXF3SyD6R27jwmdUZxEt_pQ",
+  authDomain: "job-listing-web-application.firebaseapp.com",
+  projectId: "job-listing-web-application",
+  storageBucket: "job-listing-web-application.appspot.com",
+  messagingSenderId: "657881382517",
+  appId: "1:657881382517:web:a2677bca4a37b1b527423c"
 };
-
 
 const app = initializeApp(firebaseConfig);
 
@@ -18,4 +38,5 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { auth, db };
+
 
