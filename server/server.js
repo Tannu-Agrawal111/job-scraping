627a49c734fe18a02b2db7d182e2df8298c47fd1
Tannu-Agrawal111 <tnnugrwl@gmail.com>
2026-01-29 00:47:49 +0530

    Fix Render port and env config

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ import axios from "axios";
 import cors from "cors";
 import dotenv from "dotenv";
 
-dotenv.config({ path: "./secure.env" });  // .env file load karega
+// dotenv.config({ path: "./secure.env" });  // .env file load karega
+dotenv.config();
 
 const app = express();
 app.use(cors());
